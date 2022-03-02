7a13bc597108e91f7412e60a562a22fac79faa6e
Van064886 <atadev13@gmail.com>
2022-03-02 16:14:40 +0100

    Amélioration du code pour lire les entrées de l'utilisateur

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 from gtts import gTTS
 import os
 
+# Récupération du texte de l'utilisateur
+userInput = input("Entrez votre texte :\n")
+
 # Ouverture du fichier texte et récupération de son contenu dans une variable
-f = open("text.txt")
-x = f.read()
+f = open("text.txt", "w")
+f.write(userInput)
+f.close()
+
+f1 = open("text.txt")
+x = f1.read()
 
 # Définition de la langue du fichier texte
 langue = 'fr'
@@ -14,6 +21,7 @@ audio = gTTS(text=x, lang=langue, slow=False)
 """ On sauvegarde l'audio sous forme de fichier wav et on procède à la lecture
     - La lecture dépend de l'OS utilisé.Étant sous linux j'ai utilisé des modules qui
       permettent d'effectuer une lecture audio en mode console 
+        . sudo apt-get install sox && sudo apt-get install libsox-fmt-all
     - Sous windows il s'agira juste de rentrer dans votre terminal powershell pour compiler le code
       et d'enlever le mot play de la commande"""
 audio.save("audio.mp3")
